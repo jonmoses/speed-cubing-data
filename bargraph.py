dc1c7d4c7c87d23e0c2ed3d27cf83272af88162c
Jonathan Moses <jon.moses2000@gmail.com>
2019-02-13 10:05:18 -0700

    Added mean median and fastest slowest to bargraph readout

--- bargraph.py
+++ bargraph.py
@@ -16,7 +16,7 @@ def main():
         for line in filestream:
             time_data.append(float(line.rstrip()))
 
-    plt.hist(bins=[x / 2 for x in range(44, 91)], x=time_data, color="red",
+    plt.hist(bins=[x / 2 for x in range(int(2 * min(time_data)), int(2 * max(time_data)))], x=time_data, color="red",
              rwidth=.6, label="solve time", range=.5)
     plt.legend()
     plt.title("Histogram of solves")
@@ -24,5 +24,13 @@ def main():
     plt.xlabel("Solve time (.5 second steps)")
     plt.show()
 
+    print("Fastest time: %.3f" % min(time_data))
+    print("Slowest time: % .3f" % max(time_data))
+    total = 0
+    for i in time_data:
+        total += float(i)
+    print("Mean: %.3f" % (total / len(time_data)))
     print("Standard Deviation: %.3f" % stat.stdev(time_data))
+    print("Median: %.3f" % (time_data[len(time_data) // 2]))
+
 main()
